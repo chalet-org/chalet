75be7bc1f9262a553bde3b70768b1bbbcd869a66
Andrew King <pepsiotaku@gmail.com>
2021-07-09 23:13:45 -0400

    fixed weird bash in windows terminal edge case

--- src/Terminal/OSTerminal.cpp
+++ src/Terminal/OSTerminal.cpp
@@ -61,7 +61,7 @@ void OSTerminal::initialize()
 void OSTerminal::reset()
 {
 #if defined(CHALET_WIN32)
-	if (!Environment::isBashOrWindowsConPTY() && Output::ansiColorsSupportedInComSpec())
+	if (Output::ansiColorsSupportedInComSpec())
 	{
 		HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
 		if (hOut != INVALID_HANDLE_VALUE)
