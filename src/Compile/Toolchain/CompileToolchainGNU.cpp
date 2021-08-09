45906ac359751e48b50d194e2e5a3a53870d367e
Andrew King <pepsiotaku@gmail.com>
2021-08-09 17:16:31 -0400

    alt solution for ISO C empty translation unit issue

--- src/Compile/Toolchain/CompileToolchainGNU.cpp
+++ src/Compile/Toolchain/CompileToolchainGNU.cpp
@@ -79,7 +79,7 @@ bool CompileToolchainGNU::initialize()
 
 #include "{pchIncludeAbsolute}"
 
-typedef int _chalet_build_precompiled_header_iso_compilers_workaround;
+#include "linked.h"
 )cpp",
 				FMT_ARG(pchIncludeAbsolute));
 
