b8b6ab19be304f06c7f9c5d869816e0f1a6c24ed
Andrew King <pepsiotaku@gmail.com>
2021-10-05 19:07:47 -0400

    chalet-completion.sh fix

--- scripts/chalet-completion.sh
+++ scripts/chalet-completion.sh
@@ -9,22 +9,22 @@ _chalet_completions()
 	case "${prev}" in
 	-c|--configuration)
 		COMPREPLY=($(compgen -W "$(chalet list --type configurations)" -- $cur))
-	;;
+		;;
 	-t|--toolchain)
 		COMPREPLY=($(compgen -W "$(chalet list --type all-toolchains)" -- $cur))
-	;;
+		;;
 	-a|--arch)
 		COMPREPLY=($(compgen -W "$(chalet list --type architectures)" -- $cur))
-	;;
+		;;
 	get|getkeys|set|unset)
 		_CMDS[COMP_CWORD-1]=getkeys
 		_CMDS[COMP_CWORD]="${cur//\\\\./\\.}"
 		_RESP=$(${_CMDS[@]})
-		COMPREPLY=($(compgen -W "${_RESP//\\\\./\\\\\\\\.}"))
-	;;
+		COMPREPLY=($(compgen -W "${_RESP//\\\\./\\\\\\\\.}" -- $cur))
+		;;
 	*)
 		COMPREPLY=($(compgen -W "$(chalet list --type commands)" -- $cur))
-	;;
+		;;
 	esac
 }
 
