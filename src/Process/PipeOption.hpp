11c3483ad781ad11c98a47411b1ebeaf52eb9b85
Andrew King <pepsiotaku@gmail.com>
2021-11-11 12:36:56 -0500

    removed PipeOption notes

--- src/Process/PipeOption.hpp
+++ src/Process/PipeOption.hpp
@@ -8,18 +8,13 @@
 
 namespace chalet
 {
-// maps to subprocess::PipeOption (without exposing the api)
 enum class PipeOption : int
 {
-	Inherit, ///< Inherits current process handle
-	StdOut,	 ///< Redirects to stdout
-	StdErr,	 ///< redirects to stderr
-	/** Redirects to provided pipe. You can open /dev/null. Pipe handle
-		that you specify will be made inheritable and closed automatically.
-	*/
-	Specific,
-	Pipe, ///< Redirects to a new handle created for you.
-	Close ///< Troll the child by providing a closed pipe.
+	Inherit,
+	StdOut,
+	StdErr,
+	Pipe,
+	Close
 };
 
 }
