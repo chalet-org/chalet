4aa8e58d3798bd376e9115dadb5130610c9632c2
Andrew King <pepsiotaku@gmail.com>
2022-03-29 21:04:28 -0400

    update version

--- src/Arguments/ArgumentParser.cpp
+++ src/Arguments/ArgumentParser.cpp
@@ -271,7 +271,7 @@ bool ArgumentParser::showHelp()
 /*****************************************************************************/
 bool ArgumentParser::showVersion()
 {
-	std::string version = "Chalet version 0.3.4";
+	std::string version = "Chalet version 0.3.5";
 	std::cout.write(version.data(), version.size());
 	std::cout.put(std::cout.widen('\n'));
 	std::cout.flush();
