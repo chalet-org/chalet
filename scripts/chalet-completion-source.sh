c1b0cd2c42b5cadfe470832510d9726da0930ab9
Andrew King <pepsiotaku@gmail.com>
2022-09-17 20:11:47 -0400

    chalet-completion-source.sh can't be used on macos (wonderful)

--- scripts/chalet-completion-source.sh
+++ scripts/chalet-completion-source.sh
@@ -1,15 +1,20 @@
 #!/usr/bin/env sh
 
-_CD=$(dirname -- "$0")
-_CURSHELL=$(basename $(readlink /proc/$$/exe))
-if [[ $_CURSHELL == "zsh" ]]; then
-	source $_CD/chalet-completion.zsh
-elif [[ $_CURSHELL == "bash" ]]; then
-	source $_CD/chalet-completion.bash
-elif [[ $_CURSHELL == "sh" ]]; then
-	source $_CD/chalet-completion.sh
+if [[ $OSTYPE == 'darwin'* ]]; then
+	echo "This script isn't supported in macOS. Use 'source' to the completion script of your shell."
 else
-	echo "chalet-completion-source.sh Error: Unsupported shell \"$_CURSHELL\" - Please open an issue here: https://github.com/chalet-org/chalet/issues"
+	_CURSHELL=$(basename $(readlink /proc/$$/exe))
+	_CD=$(dirname "$1")
+
+	if [[ $_CURSHELL == "zsh" ]]; then
+		source $_CD/chalet-completion.zsh
+	elif [[ $_CURSHELL == "bash" ]]; then
+		source $_CD/chalet-completion.bash
+	elif [[ $_CURSHELL == "sh" ]]; then
+		source $_CD/chalet-completion.sh
+	else
+		echo "chalet-completion-source.sh Error: Unsupported shell \"$_CURSHELL\" - Please open an issue here: https://github.com/chalet-org/chalet/issues"
+	fi
+	unset _CURSHELL
+	unset _CD
 fi
-unset _CURSHELL
-unset _CD
