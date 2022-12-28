chalet-org/chalet

commit c17ef9bd45d946d3dfce31f388d0c4972b873179

bugfix: correctly track state changes in build targets - files, flags etc, bump version

src/State/BuildState.cpp
@@ -1176,21 +1176,26 @@ std::string BuildState::getUniqueIdForState() const
 	const auto targetArch = inputs.getArchWithOptionsAsString(info.targetArchitectureTriple());
 	const auto envId = m_impl->environment->identifier() + toolchain.version();
 	const auto& buildConfig = info.buildConfiguration();
-	const auto extensions = String::join(paths.allFileExtensions(), '_');
 
-	int showCmds = 0;
+	bool showCmds = false;
 	if (toolchain.strategy() != StrategyType::Ninja)
 	{
-		showCmds = Output::showCommands() ? 1 : 0;
+		showCmds = Output::showCommands();
 	}
 
-	int dumpAssembly = 0;
+	bool dumpAssembly = false;
 	/*if (m_impl->environment->type() == ToolchainType::VisualStudio)
 	{
-		dumpAssembly = info.dumpAssembly() ? 1 : 0;
+		dumpAssembly = info.dumpAssembly();
 	}*/
 
-	ret = fmt::format("{}_{}_{}_{}_{}_{}_{}", hostArch, targetArch, envId, buildConfig, showCmds, dumpAssembly, extensions);
+	std::string targetHash;
+	for (auto& target : targets)
+	{
+		targetHash += target->getHash();
+	}
+
+	ret = Hash::getHashableString(hostArch, targetArch, envId, buildConfig, showCmds, dumpAssembly, targetHash);
 
 	return Hash::string(ret);
 }

CMakeLists.txt
@@ -1,7 +1,7 @@
 cmake_minimum_required(VERSION 3.16)
 
 project(chalet-debug
-	VERSION 0.5.12
+	VERSION 0.5.13
 	DESCRIPTION "A JSON-based project & build tool"
 	HOMEPAGE_URL "https://www.chalet-work.space"
 	LANGUAGES CXX

src/State/BuildPaths.cpp
@@ -92,23 +92,6 @@ void BuildPaths::populateFileList(const SourceTarget& inProject)
 		return;
 
 	auto files = getFiles(inProject);
-
-	bool extChanged = false;
-	for (auto& file : files->list)
-	{
-		auto ext = String::getPathSuffix(file);
-		if (ext.empty())
-			continue;
-
-		if (List::addIfDoesNotExist(m_allFileExtensions, std::move(ext)))
-		{
-			extChanged = true;
-		}
-	}
-
-	if (extChanged)
-		std::sort(m_allFileExtensions.rbegin(), m_allFileExtensions.rend());
-
 	m_fileList.emplace(inProject.name(), std::move(files));
 }
 
@@ -211,10 +194,6 @@ std::string BuildPaths::getExternalBuildDir(const std::string& inName) const
 }
 
 /*****************************************************************************/
-const StringList& BuildPaths::allFileExtensions() const noexcept
-{
-	return m_allFileExtensions;
-}
 const std::string& BuildPaths::cxxExtension() const
 {
 	if (m_cxxExtension.empty())

src/State/BuildPaths.hpp
@@ -35,7 +35,6 @@ struct BuildPaths
 	std::string getExternalDir(const std::string& inName) const;
 	std::string getExternalBuildDir(const std::string& inName) const;
 
-	const StringList& allFileExtensions() const noexcept;
 	const std::string& cxxExtension() const;
 
 	std::string getTargetFilename(const SourceTarget& inProject) const;
@@ -90,7 +89,6 @@ private:
 	const std::string m_objectiveCppExt;
 
 	HeapDictionary<SourceGroup> m_fileList;
-	StringList m_allFileExtensions;
 
 	std::string m_buildOutputDir;
 	std::string m_currentBuildDir;

src/State/Target/CMakeTarget.cpp
@@ -10,6 +10,7 @@
 #include "State/BuildState.hpp"
 #include "State/CompilerTools.hpp"
 #include "Terminal/Commands.hpp"
+#include "Utility/Hash.hpp"
 #include "Utility/List.hpp"
 #include "Utility/String.hpp"
 
@@ -81,6 +82,17 @@ bool CMakeTarget::validate()
 	}
 
 	return result;
+}
+
+/*****************************************************************************/
+std::string CMakeTarget::getHash() const
+{
+	auto defines = String::join(m_defines);
+	auto targets = String::join(m_targets);
+
+	auto hashable = Hash::getHashableString(this->name(), m_location, m_targetFolder, m_runExecutable, m_buildFile, m_toolset, defines, targets, m_recheck, m_rebuild, m_clean);
+
+	return Hash::string(hashable);
 }
 
 /*****************************************************************************/

src/State/Target/CMakeTarget.hpp
@@ -16,6 +16,7 @@ struct CMakeTarget final : public IBuildTarget
 
 	virtual bool initialize() final;
 	virtual bool validate() final;
+	virtual std::string getHash() const final;
 
 	const StringList& defines() const noexcept;
 	void addDefines(StringList&& inList);

src/State/Target/IBuildTarget.hpp
@@ -23,6 +23,7 @@ struct IBuildTarget
 	[[nodiscard]] static BuildTarget make(const BuildTargetType inType, const BuildState& inState);
 
 	virtual bool validate() = 0;
+	virtual std::string getHash() const = 0;
 
 	virtual bool initialize();
 

src/State/Target/ProcessBuildTarget.cpp
@@ -8,7 +8,9 @@
 #include "State/BuildState.hpp"
 #include "Terminal/Commands.hpp"
 #include "Terminal/Path.hpp"
+#include "Utility/Hash.hpp"
 #include "Utility/List.hpp"
+#include "Utility/String.hpp"
 
 namespace chalet
 {
@@ -49,6 +51,16 @@ bool ProcessBuildTarget::validate()
 	}
 
 	return true;
+}
+
+/*****************************************************************************/
+std::string ProcessBuildTarget::getHash() const
+{
+	auto args = String::join(m_arguments);
+
+	auto hashable = Hash::getHashableString(this->name(), m_path, args);
+
+	return Hash::string(hashable);
 }
 
 /*****************************************************************************/

commit 7e01b13e2213d1734b1a8d39e94b4d54a55abc27

bugfix: fix huge cache path issue caused by c17ef9b

src/Cache/WorkspaceCache.cpp
@@ -199,9 +199,9 @@ void WorkspaceCache::saveSettings(const SettingsType inType)
 bool WorkspaceCache::removeStaleProjectCaches()
 {
 	const auto& cacheRef = getCacheRef(CacheType::Local);
-	StringList ids = m_cacheFile.getCacheIdsForRemoval();
+	StringList ids = m_cacheFile.getCacheIdsToNotRemove();
 
-	if (!Commands::pathExists(cacheRef) || ids.size() == 0)
+	if (!Commands::pathExists(cacheRef) || ids.empty())
 		return true;
 
 	Output::setShowCommandOverride(false);

src/Cache/WorkspaceInternalCacheFile.cpp
@@ -28,7 +28,7 @@ void WorkspaceInternalCacheFile::setBuildHash(const std::string& inValue) noexce
 {
 	m_buildHashChanged = m_buildHash != inValue;
 	m_dirty |= m_buildHashChanged;
-	m_buildHash = std::move(inValue);
+	m_buildHash = inValue;
 }
 
 /*****************************************************************************/
@@ -45,14 +45,8 @@ ExternalDependencyCache& WorkspaceInternalCacheFile::externalDependencies()
 }
 
 /*****************************************************************************/
-bool WorkspaceInternalCacheFile::setSourceCache(const std::string& inId, const StrategyType inStrategy, const bool inCheckHashChange)
+bool WorkspaceInternalCacheFile::setSourceCache(const std::string& inId, const StrategyType inStrategy)
 {
-	if (inCheckHashChange)
-		setBuildHash(inId);
-
-	if (inStrategy == StrategyType::Native)
-		List::addIfDoesNotExist(m_doNotRemoves, inId);
-
 	auto itr = m_sourceCaches.find(inId);
 	if (itr != m_sourceCaches.end())
 	{
@@ -517,24 +511,18 @@ std::string WorkspaceInternalCacheFile::getAppVersionHash(const std::string& inA
 }
 
 /*****************************************************************************/
-StringList WorkspaceInternalCacheFile::getCacheIdsForRemoval() const
+StringList WorkspaceInternalCacheFile::getCacheIdsToNotRemove() const
 {
 	StringList ret;
 	ret.emplace_back(String::getPathFilename(m_filename));
 
 	for (auto& id : m_extraHashes)
 	{
-		if (List::contains(m_doNotRemoves, id))
-			continue;
-
 		List::addIfDoesNotExist(ret, id);
 	}
 
 	for (auto& [id, _] : m_sourceCaches)
 	{
-		if (List::contains(m_doNotRemoves, id))
-			continue;
-
 		List::addIfDoesNotExist(ret, id);
 	}
 
@@ -546,9 +534,6 @@ StringList WorkspaceInternalCacheFile::getCacheIdsForRemoval() const
 		{
 			for (auto [id, _] : builds.items())
 			{
-				if (List::contains(m_doNotRemoves, id))
-					continue;
-
 				List::addIfDoesNotExist(ret, id);
 			}
 		}

src/Cache/WorkspaceInternalCacheFile.hpp
@@ -27,12 +27,13 @@ struct WorkspaceInternalCacheFile
 	bool loadExternalDependencies(const std::string& inPath);
 	bool saveExternalDependencies();
 
-	bool setSourceCache(const std::string& inId, const StrategyType inStrategy, const bool inCheckHashChange);
+	bool setSourceCache(const std::string& inId, const StrategyType inStrategy);
 	bool removeSourceCache(const std::string& inId);
 	bool removeExtraCache(const std::string& inId);
 
 	bool buildHashChanged() const noexcept;
 	bool buildFileChanged() const noexcept;
+	void setBuildHash(const std::string& inValue) noexcept;
 
 	bool forceRebuild() const noexcept;
 	void setForceRebuild(const bool inValue);
@@ -56,11 +57,9 @@ struct WorkspaceInternalCacheFile
 	SourceCache& sources() const;
 	ExternalDependencyCache& externalDependencies();
 
-	StringList getCacheIdsForRemoval() const;
+	StringList getCacheIdsToNotRemove() const;
 
 private:
-	void setBuildHash(const std::string& inValue) noexcept;
-
 	std::string getAppVersionHash(const std::string& inAppPath);
 
 	StringList m_doNotRemoves;

src/Compile/Strategy/CompileStrategyMSBuild.cpp
@@ -32,8 +32,8 @@ bool CompileStrategyMSBuild::initialize()
 		return false;
 
 	auto& cacheFile = m_state.cache.file();
-	const auto& uniqueId = m_state.uniqueId();
-	UNUSED(m_state.cache.getCachePath(uniqueId, CacheType::Local));
+	const auto& cachePathId = m_state.cachePathId();
+	UNUSED(m_state.cache.getCachePath(cachePathId, CacheType::Local));
 
 	const bool buildStrategyChanged = cacheFile.buildStrategyChanged(m_state.toolchain.strategy());
 	if (buildStrategyChanged)

src/Compile/Strategy/CompileStrategyMakefile.cpp
@@ -31,9 +31,9 @@ bool CompileStrategyMakefile::initialize()
 	if (m_initialized)
 		return false;
 
-	const auto& uniqueId = m_state.uniqueId();
+	const auto& cachePathId = m_state.cachePathId();
 	auto& cacheFile = m_state.cache.file();
-	m_cacheFolder = m_state.cache.getCachePath(uniqueId, CacheType::Local);
+	m_cacheFolder = m_state.cache.getCachePath(cachePathId, CacheType::Local);
 
 	const bool cacheExists = Commands::pathExists(m_cacheFolder);
 	const bool appVersionChanged = cacheFile.appVersionChanged();

src/Compile/Strategy/CompileStrategyNative.cpp
@@ -40,8 +40,8 @@ bool CompileStrategyNative::initialize()
 		return false;
 
 	auto& cacheFile = m_state.cache.file();
-	const auto& uniqueId = m_state.uniqueId();
-	UNUSED(m_state.cache.getCachePath(uniqueId, CacheType::Local));
+	const auto& cachePathId = m_state.cachePathId();
+	UNUSED(m_state.cache.getCachePath(cachePathId, CacheType::Local));
 
 	const bool buildStrategyChanged = cacheFile.buildStrategyChanged(m_state.toolchain.strategy());
 	if (buildStrategyChanged)

src/Compile/Strategy/CompileStrategyNinja.cpp
@@ -31,10 +31,10 @@ bool CompileStrategyNinja::initialize()
 	if (m_initialized)
 		return false;
 
-	const auto& uniqueId = m_state.uniqueId();
+	const auto& cachePathId = m_state.cachePathId();
 
 	auto& cacheFile = m_state.cache.file();
-	m_cacheFolder = m_state.cache.getCachePath(uniqueId, CacheType::Local);
+	m_cacheFolder = m_state.cache.getCachePath(cachePathId, CacheType::Local);
 	m_cacheFile = fmt::format("{}/build.ninja", m_cacheFolder);
 
 	const bool cacheExists = Commands::pathExists(m_cacheFolder) && Commands::pathExists(m_cacheFile);

src/State/BuildState.cpp
@@ -154,9 +154,9 @@ bool BuildState::doBuild(const CommandRoute& inRoute, const bool inShowSuccess)
 }
 
 /*****************************************************************************/
-const std::string& BuildState::uniqueId() const noexcept
+const std::string& BuildState::cachePathId() const noexcept
 {
-	return m_uniqueId;
+	return m_cachePathId;
 }
 
 /*****************************************************************************/
@@ -283,8 +283,9 @@ bool BuildState::initializeToolchain()
 	Timer timer;
 
 	auto& cacheFile = m_impl->centralState.cache.file();
-	m_uniqueId = getUniqueIdForState(); // this will be incomplete by this point, but wee need it when the toolchain initializes
-	cacheFile.setSourceCache(m_uniqueId, StrategyType::Native, false);
+	generateUniqueIdForState(); // this will be incomplete by this point, but wee need it when the toolchain initializes
+	cacheFile.setBuildHash(m_uniqueId);
+	cacheFile.setSourceCache(m_cachePathId, StrategyType::Native);
 
 	auto onError = [this]() -> bool {
 		const auto& targetArch = m_impl->environment->type() == ToolchainType::GNU ?
@@ -397,8 +398,9 @@ bool BuildState::initializeBuild()
 	}
 
 	auto& cacheFile = m_impl->centralState.cache.file();
-	m_uniqueId = getUniqueIdForState();
-	cacheFile.setSourceCache(m_uniqueId, toolchain.strategy(), true);
+	generateUniqueIdForState();
+	cacheFile.setBuildHash(m_uniqueId);
+	cacheFile.setSourceCache(m_cachePathId, toolchain.strategy());
 
 	Diagnostic::printDone(timer.asString());
 
@@ -1169,9 +1171,8 @@ bool BuildState::replaceVariablesInString(std::string& outString, const IDistTar
 }
 
 /*****************************************************************************/
-std::string BuildState::getUniqueIdForState() const
+void BuildState::generateUniqueIdForState()
 {
-	std::string ret;
 	const auto& hostArch = info.hostArchitectureString();
 	const auto targetArch = inputs.getArchWithOptionsAsString(info.targetArchitectureTriple());
 	const auto envId = m_impl->environment->identifier() + toolchain.version();
@@ -1195,9 +1196,13 @@ std::string BuildState::getUniqueIdForState() const
 		targetHash += target->getHash();
 	}
 
-	ret = Hash::getHashableString(hostArch, targetArch, envId, buildConfig, showCmds, dumpAssembly, targetHash);
+	// Note: no targetHash
+	auto hashable = Hash::getHashableString(hostArch, targetArch, envId, buildConfig, showCmds, dumpAssembly);
+	m_cachePathId = Hash::string(hashable);
 
-	return Hash::string(ret);
+	// Unique ID is used by the internal cache to determine if the build files need to be updated
+	auto hashableTargets = Hash::getHashableString(m_cachePathId, targetHash);
+	m_uniqueId = Hash::string(hashableTargets);
 }
 
 }

src/State/BuildState.hpp
@@ -43,7 +43,7 @@ public:
 
 	bool replaceVariablesInString(std::string& outString, const IBuildTarget* inTarget, const bool inCheckHome = true, const std::function<std::string(std::string)>& onFail = nullptr) const;
 	bool replaceVariablesInString(std::string& outString, const IDistTarget* inTarget, const bool inCheckHome = true, const std::function<std::string(std::string)>& onFail = nullptr) const;
-	const std::string& uniqueId() const noexcept;
+	const std::string& cachePathId() const noexcept;
 
 	AncillaryTools& tools;
 	WorkspaceCache& cache;
@@ -76,8 +76,9 @@ private:
 	void enforceArchitectureInPath();
 	void enforceArchitectureInPath(std::string& outPathVariable);
 
-	std::string getUniqueIdForState() const;
+	void generateUniqueIdForState();
 
+	std::string m_cachePathId;
 	std::string m_uniqueId;
 };
 }