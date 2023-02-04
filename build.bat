03cdead15b0ef0a33847981fd560e18867d9673d
Andrew King <pepsiotaku@gmail.com>
2023-02-04 15:41:57 -0500

    tweak build.bat to have same output as build.sh

--- build.bat
+++ build.bat
@@ -1,7 +1,18 @@
+@echo off
+
 set CWD=%CD%
 
-set BUILD_CONFIGURATION=Debug
+set BUILD_CONFIGURATION=%1
 set BUILD_FOLDER="build/msvc_%BUILD_CONFIGURATION%"
+
+IF [%1] == [] (
+	echo "Error: please provide a build configuration (Debug / Release)"
+	exit 1
+)
 
 cmake -G "Visual Studio 17 2022" -DCMAKE_BUILD_TYPE=%BUILD_CONFIGURATION% -S %CWD% -B %BUILD_FOLDER%
 cmake --build %BUILD_FOLDER%
+
+echo:
+echo ===============================================================================
+echo:
