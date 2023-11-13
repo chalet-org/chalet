0f21c3902af1efbad69ba579fe8f9e64fd004029
Andrew King <pepsiotaku@gmail.com>
2023-11-12 19:17:38 -0500

    vscode export: don't generate a c_cpp_properties file if there are no valid targets

--- src/Export/VSCode/VSCodeCCppPropertiesGen.cpp
+++ src/Export/VSCode/VSCodeCCppPropertiesGen.cpp
@@ -113,7 +113,7 @@ bool VSCodeCCppPropertiesGen::saveToFile(const std::string& inFilename) const
 	}
 
 	if (!hasProjects)
-		return false;
+		return true;
 
 	if (!cStandard.empty())
 		config["cStandard"] = std::move(cStandard);
