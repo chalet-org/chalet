89f366c82523d1784f6f8f715585f0fee36652a1
Andrew King <pepsiotaku@gmail.com>
2023-11-22 23:29:45 -0500

    Files::pathExists - use GetFileAttributes on Windows

--- src/System/Files.cpp
+++ src/System/Files.cpp
@@ -586,8 +586,10 @@ bool Files::rename(const std::string& inFrom, const std::string& inTo, const boo
 bool Files::pathExists(const std::string& inFile)
 {
 #if defined(CHALET_WIN32)
-	std::error_code ec;
-	return fs::exists(inFile, ec);
+	// std::error_code ec;
+	// return fs::exists(inFile, ec);
+	DWORD dwAttrib = ::GetFileAttributesA(inFile.c_str());
+	return dwAttrib != INVALID_FILE_ATTRIBUTES;
 #else
 	return stat(inFile.c_str(), &statBuffer) == 0;
 #endif
